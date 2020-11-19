8d1f605346fdab4fbaa887df59c54f24ac0c98ef
Joel Rocha <joelxr@gmail.com>
2020-11-18 23:05:42 -0300

    Adds main layout

---
+++ webapp/src/app/panel/panel.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-panel',
+  templateUrl: './panel.component.html',
+  styleUrls: ['./panel.component.scss']
+})
+export class PanelComponent implements OnInit {
+
+  @Input()
+  title: string;
+
+  @Input()
+  icon: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
