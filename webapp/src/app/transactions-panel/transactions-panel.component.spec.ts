9fc99e5bfd269df5bad85ce7c870b9f4ca7fa476
Joel Rocha <joelxr@gmail.com>
2020-11-20 11:41:51 -0300

    Fixes tests

--- webapp/src/app/transactions-panel/transactions-panel.component.spec.ts
+++ webapp/src/app/transactions-panel/transactions-panel.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { TransactionsPanelComponent } from './transactions-panel.component';
 
@@ -8,7 +9,8 @@ describe('TransactionsPanelComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TransactionsPanelComponent ]
+      imports: [ HttpClientTestingModule ],
+      declarations: [ TransactionsPanelComponent ],
     })
     .compileComponents();
   });
