506d21a6b32109445ba3df52aa441221013cfc0c
Joel Rocha <joelxr@gmail.com>
2020-11-20 10:39:47 -0300

    Fixes sorting

--- webapp/src/app/transactions-sorter/transactions-sorter.component.ts
+++ webapp/src/app/transactions-sorter/transactions-sorter.component.ts
@@ -19,12 +19,20 @@ export class TransactionsSorterComponent implements OnInit {
   ngOnInit(): void {}
 
   updateSortingPreference(preference: SortingPreference): void {
+    const previousPreference = this.value
+      .find(sortingPreference => sortingPreference.order !== SortingOrder.NONE && sortingPreference.prop !== preference.prop);
+
+    const previousOrder = previousPreference ? previousPreference.order : SortingOrder.DESC;
+
+    // Reset others to NONE
     this.value
       .filter(sortingPreference => sortingPreference.prop !== preference.prop)
       .forEach(sortingPreference => sortingPreference.order = SortingOrder.NONE);
 
     if (preference.order === SortingOrder.NONE)  {
-      preference.order = SortingOrder.DESC;
+      preference.order = previousOrder === SortingOrder.ASC
+        ? SortingOrder.ASC
+        : SortingOrder.DESC;
     } else if (preference.order === SortingOrder.DESC) {
       preference.order = SortingOrder.ASC;
     } else if (preference.order === SortingOrder.ASC) {
